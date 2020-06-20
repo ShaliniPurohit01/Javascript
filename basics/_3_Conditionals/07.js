fab6ce9932810df0cdcdb3091496b43498e4a81e
Shalini Purohit <purohit.shaily001@gmail.com>
2020-06-20 16:23:09 +0530

    switch statement

---
+++ basics/_3_Conditionals/07.js
@@ -0,0 +1,9 @@
+// ternery operator
+
+var name = "shaily";
+var age = 23;
+
+// Ternery Operator
+age <= 18
+  ? console.log(name + " is a girl.")
+  : console.log(name + " is a woman.");
---
+++ basics/_3_Conditionals/08.js
@@ -0,0 +1,43 @@
+// switch statement
+
+var job1 = "cop";
+//1
+// switch (job1) {
+//   case "teacher":
+//     console.log("The job is Teaching");
+//   case "cop":
+//     console.log("he is police");
+//     break;
+//   case "programmer":
+//     console.log(" he is programmer");
+// }
+
+// var job2 = "developer";
+// //2
+// switch (job2) {
+//   case "cop":
+//     console.log("he is police");
+
+//   case "teacher":
+//     console.log("The job is Teaching");
+//     break;
+//   case "programmer":
+//     console.log(" he is programmer");
+//     break;
+// }
+
+var job3 = "developer";
+//3
+switch (job3) {
+  case "teacher":
+    console.log("The job is Teaching");
+    break;
+  case "cop":
+    console.log("he is police");
+    break;
+  case "programmer":
+    console.log(" he is programmer");
+    break;
+  default:
+    console.log("job is not mentioned here!!");
+}
