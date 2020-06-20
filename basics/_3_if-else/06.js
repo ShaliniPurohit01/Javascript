a3d52e85f705e49db59cd99cb28b52b299a7c4a9
Shalini Purohit <purohit.shaily001@gmail.com>
2020-06-20 16:08:25 +0530

    if-else statements with boolean logics

---
+++ basics/_3_if-else/06.js
@@ -0,0 +1,12 @@
+// boolean logics
+
+var name = "shaily";
+var age = 23;
+
+if (age < 13) {
+  console.log(name + " is a girl.");
+} else if (age >= 13 && age <= 18) {
+  console.log(name + " is a teenager.");
+} else if (age > 18) {
+  console.log(name + " is a Woman.");
+}
