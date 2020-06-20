35bd0195ba1b0073d7569ea86c33b26186b40ba8
Shalini Purohit <purohit.shaily001@gmail.com>
2020-06-20 15:56:27 +0530

    if-else statements

---
+++ basics/_3_if-else/05.js
@@ -0,0 +1,25 @@
+var firstName = "shaily";
+var civilStatus = "single";
+
+//if
+if (civilStatus === "married") {
+  console.log(firstName + " is married !");
+}
+
+//if else
+var isMarried = false;
+if (isMarried) {
+  console.log(firstName + " is married !");
+} else {
+  console.log(firstName + " will hopefully marry soon !");
+}
+
+//using operators in the condition block
+var ageX = 23;
+var ageY = 40;
+
+if (ageY > ageX) {
+  console.log("y is older than x");
+} else {
+  console.log("x is older than y");
+}
