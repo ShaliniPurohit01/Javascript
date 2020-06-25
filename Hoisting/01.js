d63c139333bba1d26d24102359ae55f554b8a927
Shalini Purohit <purohit.shaily001@gmail.com>
2020-06-25 09:38:30 +0530

    Hoisting in function and variables

---
+++ Hoisting/01.js
@@ -0,0 +1,40 @@
+// hoisting
+
+//functions
+function calculateAge(year) {
+  console.log(2020 - year);
+}
+calculateAge(1990);
+
+calculateAge(1996);
+function calculateAge(year) {
+  console.log(2020 - year);
+}
+
+var retirement = function (year) {
+  console.log(65 - (2020 - year));
+};
+retirement(1990);
+
+// retirement(1990);
+// var retirement = function (year) {
+//   console.log(65 - (2020 - year));
+// };
+
+// variables
+console.log(age); // undefined
+var age = 23;
+console.log(age); //23
+
+// var x = 60;
+// console.log(x);//  error
+
+var agee = 23;
+function foo() {
+  console.log(age); // undefined
+
+  var age = 89;
+  console.log(age); //89
+}
+foo();
+console.log(age); //23
