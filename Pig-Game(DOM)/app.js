34b867df3c629ffb2058b6653b65e5239ddd6518
Shalini Purohit <purohit.shaily001@gmail.com>
2020-07-05 15:53:22 +0530

    DRY principle and implementing hold function in pig game

--- Pig-Game(DOM)/app.js
+++ Pig-Game(DOM)/app.js
@@ -44,23 +44,51 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
   if (dice !== 1) {
     //add score
     roundScore += dice;
-    document.querySelector(
-      "#current-" + activePlayer
-    ).textContent = roundScorel;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
   } else {
     // next player
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    roundScore = 0;
-
-    document.getElementById("current-0").textContent = "0";
-    document.getElementById("current-1").textContent = "0";
-
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-
-    // document.querySelector(".player-0-panel").classList.remove("active");
-    // document.querySelector(".player-1-panel").classList.add("active");
-
-    document.querySelector(".dice").style.display = "none";
+    nextPlayer();
   }
 });
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  //add current score to GLOBAL score
+  scores[activePlayer] += roundScore;
+
+  //update the UI
+  document.querySelector("#score-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  //Check if player won the game
+  if (scores[activePlayer] >= 20) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!!";
+    document.querySelector(".dice").style.display = "none";
+
+    document
+      .querySelector(".player=" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player=" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    nextPlayer();
+  }
+  //next player
+  nextPlayer();
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  // document.querySelector(".player-0-panel").classList.remove("active");
+  // document.querySelector(".player-1-panel").classList.add("active");
+
+  document.querySelector(".dice").style.display = "none";
+}
