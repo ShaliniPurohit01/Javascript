dc30f4f1470ad1e5eae2fb6e42db469e42cc79c4
Shalini Purohit <purohit.shaily001@gmail.com>
2020-07-05 14:26:50 +0530

    updating scores and changing the active player

--- Pig-Game(DOM)/app.js
+++ Pig-Game(DOM)/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 // dice = Math.floor(Math.random() * 6) + 1;
 
@@ -39,5 +39,28 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
   var diceDom = document.querySelector(".dice");
   diceDom.style.display = "block";
   diceDom.src = "dice-" + dice + ".png";
+
   //3. update the round score if the rolled number was not a 1
+  if (dice !== 1) {
+    //add score
+    roundScore += dice;
+    document.querySelector(
+      "#current-" + activePlayer
+    ).textContent = roundScorel;
+  } else {
+    // next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // document.querySelector(".player-0-panel").classList.remove("active");
+    // document.querySelector(".player-1-panel").classList.add("active");
+
+    document.querySelector(".dice").style.display = "none";
+  }
 });
